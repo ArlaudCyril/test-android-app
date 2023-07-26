46dd4e519d0ab9dc69d0f350db05a120b5f1d75a
Test420 <ankit22jindal@gmail.com>
2023-07-26 20:37:32 +0530

    deposit started

--- app/src/main/java/com/au/lyber/ui/portfolio/bottomSheetFragment/PortfolioThreeDots.kt
+++ app/src/main/java/com/au/lyber/ui/portfolio/bottomSheetFragment/PortfolioThreeDots.kt
@@ -102,13 +102,13 @@ class PortfolioThreeDots(private val listenItemClicked: (String, String) -> Unit
             it.root.updatePadding(left = 0, right = 0)
             it.ivItem.setImageResource(R.drawable.ic_deposit)
             it.ivEndIcon.setImageResource(R.drawable.ic_right_arrow_grey)
-            if (this.typePopUp == "AssetPopUp") {
+         //   if (this.typePopUp == "AssetPopUp") {
                 it.tvStartTitle.text = "Deposit"
                 it.tvStartSubTitle.text = "Add ${viewModel.selectedAsset?.id?.uppercase()} on Lyber"
-            } else {
-                it.tvStartTitle.text = "Deposit"
-                it.tvStartSubTitle.text = "Add money on Lyber"
-            }
+           // } else {
+             //   it.tvStartTitle.text = "Deposit"
+               // it.tvStartSubTitle.text = "Add money on Lyber"
+            //}
         }
 
         binding.llSell.let {
